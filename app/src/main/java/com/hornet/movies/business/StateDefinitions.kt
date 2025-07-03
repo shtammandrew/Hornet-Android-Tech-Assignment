a455834731e4636398a4d976547cb4dc33985fb7
andrewshtamm <shtammandrew@gmail.com>
2025-07-03 23:37:25 +0400

    ViewState, SideEffects and Actions for the StateMachine

--- app/src/main/java/com/hornet/movies/business/StateDefinitions.kt
+++ app/src/main/java/com/hornet/movies/business/StateDefinitions.kt
@@ -18,3 +18,32 @@ data class StateMachineState(
     val reachedEnd: Boolean = false,
     val currentPage: Int = 1
 )
+
+sealed interface UserAction {
+    data object LoadInitial : UserAction
+    data object LoadMore : UserAction
+    data class ToggleExpand(val movieId: Int) : UserAction
+    data class ShowPoster(val url: String?) : UserAction
+    data object DismissPoster : UserAction
+    data class SelectGenre(val genreId: Int?) : UserAction
+}
+
+sealed interface InternalAction {
+    data class MoviesLoaded(val newMovies: List<Movie>) : InternalAction
+    data class MovieDetailsLoaded(val movieId: Int, val details: MovieDetails) : InternalAction
+    data class GenresLoaded(val genres: List<Genre>) : InternalAction
+}
+
+sealed interface SideEffect {
+    data object FetchNextMovies : SideEffect
+    data object FetchGenres : SideEffect
+    data class FetchMovieDetails(val movieId: Int) : SideEffect
+}
+
+/** State of the ui screen */
+data class ViewState(
+    val movies: List<MovieUi>,
+    val genreStats: List<GenreStat>,
+    val selectedPoster: String?,
+    val isLoading: Boolean
+)
